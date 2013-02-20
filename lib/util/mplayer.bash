cc19d624101109ad15aeb3a3ad7fee5ff33310eb
Brent Sanders <git@thoughtnoise.net>
2013-02-20 04:23:26 -0800

    added menu-selectable mode state to the "m" mplayer_launcher command

--- lib/util/mplayer.bash
+++ lib/util/mplayer.bash
@@ -49,24 +49,12 @@ _mplayer_launcher() {
 #######################################################################
 # exported wrappers
 
-m() {
-    local MP_BINOPT=''
-    _mplayer_launcher "$@"
+mplay() {
+     local MP_BINOPT=''
+     _mplayer_launcher "$@"
 }
-mm() {
+mverbose() {
     local MP_BINOPT='-v'
-    _mplayer_launcher "$@"
-}
-m3d() {
-    local MP_BINOPT='-profile m.3d -v'
-    _mplayer_launcher "$@"
-}
-mdbg()   {
-    local MP_BINOPT='-v -v -v -msglevel demux=0'
-    _mplayer_launcher "$@"
-}
-mn() {
-    local MP_BIN="mplayer" MP_NAMEPAD="-" MP_BINOPT="-quiet"
     _mplayer_launcher "$@"
 }
 mcomp() {
@@ -77,16 +65,67 @@ mcompmore() {
     local MP_BINOPT='-profile compmore'
     _mplayer_launcher "$@"
 }
+m3d() {
+    local MP_BINOPT='-profile m.3d -v'
+    _mplayer_launcher "$@"
+}
+# mdbg()   {
+#     local MP_BINOPT='-v -v -v -msglevel demux=0'
+#     _mplayer_launcher "$@"
+# }
+# mn() {
+#     local MP_BIN="mplayer" MP_NAMEPAD="-" MP_BINOPT="-quiet"
+#     _mplayer_launcher "$@"
+# }
 
 
 if ! is_cmd mp ; then
     alias mp="mplayer2"
 fi
 
-if ! is_cmd mc ; then
-    alias mc="mcomp"
-fi
+declare MPLAYER_M_MODE=m
+declare -a MPLAYER_M_MODES=( play player2 comp compmore verbose 3d )
 
-if ! is_cmd mcm ; then
-    alias mcm="mcompmore"
-fi
+m() {
+    case ${MPLAYER_M_MODE} in
+        play | player2 | comp | compmore | verbose | 3d)
+            local cmd="m${MPLAYER_M_MODE}"
+            $cmd "$@"
+            ;;
+        *)
+            if [[ -n "${MPLAYER_M_MODE}" ]] ; then
+                dwarn "not a valid mode: MPLAYER_M_MODE=\"${MPLAYER_M_MODE}\""
+                dwarn "falling back to the basic \"play\" mode"
+            fi
+            mplay "$@"
+            ;;
+    esac
+}
+
+mmode() {
+    local m="$1"
+    if (( $# < 1 )) ; then
+        local oldPS3="${PS3}"
+        PS3="mplayer profile for the \"m\" launcher: "
+        select m in "${MPLAYER_M_MODES[@]}" ; do
+            if [[ -z "${m}" ]] ; then
+                m="${REPLY}"
+            fi
+            break;
+        done
+        PS3="${oldPS3}"
+    fi
+    case ${m} in
+        play | player2 | comp | compmore | verbose | 3d)
+            MPLAYER_M_MODE="${m}"
+            ;;
+        *)  MPLAYER_M_MODE="play"
+            if [[ -n "${m}" ]] ; then
+                dwarn "unknown mode \"${m}\""
+                dwarn "resetting back to basic \"${MPLAYER_M_MODE}\" mode"
+            fi
+            ;;
+    esac
+    dinfo "MPLAYER_M_MODE=\"${MPLAYER_M_MODE}\""
+}
+complete -W "${MPLAYER_M_MODES[*]}" mmode
